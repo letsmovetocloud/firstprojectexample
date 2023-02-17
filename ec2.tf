54195158dd3338539bf7790cd22af5c3555f0221
letsmovetocloud <letsmovetocloud@gmail.com>
2023-02-17 07:14:29 +0530

    code modifications completed

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source  = "hashicorp/azure"
       
     }
   }
@@ -11,13 +11,13 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" 
-  region  = "ap-south-1"
+  region  = "us-east-1"
 }
 
 #Resource Block
 resource "aws_instance" "ec2testserver" {
   ami           = "ami-041db4a969fe3eb68" 
-  instance_type = "t3.micro"
+  instance_type = "b2S"
 
 }
 
